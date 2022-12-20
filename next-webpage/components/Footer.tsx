1b12fc50afce299cee12e0a9e74c4aa89e4b715e
Evan Price <eprice@aforza.com>
2022-12-20 16:53:45 +0000

    wip content

--- next-webpage/components/Footer.tsx
+++ next-webpage/components/Footer.tsx
@@ -1,7 +1,7 @@
 import styles from "../styles/Home.module.css";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer Info</footer>;
+  return <footer className={styles.footer}></footer>;
 };
 
 export default Footer;
--- next-webpage/pages/index.tsx
+++ next-webpage/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Fade, Slide } from "react-awesome-reveal";
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -17,35 +19,113 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.headerWrapper}>
           <h1 className={styles.title}>Hey There! I&apos;m Evan!</h1>
-          <div
-            style={{
-              borderRadius: "50%",
-              height: 300,
-              width: 300,
-            }}
-          >
-            <Image
-              height={200}
-              width={300}
-              src={"/headshot.jpeg"}
-              alt={"headshot"}
-              style={{ borderRadius: "50%" }}
-            ></Image>
-          </div>
+          <Image
+            src={"/headshot.jpg"}
+            alt={"headshot"}
+            height={175}
+            width={175}
+            className={styles.headerImg}
+          ></Image>
           <p className={styles.description}>
-            Seasoned software developer <span className="neon">|</span> Proven
-            leader <span className="neon">|</span> Proud dad
+            Seasoned software developer | Proven leader | Proud dad
           </p>
         </div>
         <div>
-          <h2>Professional Achievements</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-          <h2>Personal Projects</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-          <h2>Links</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-          <h2>Fun Stuff</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <Fade>
+            <div className={styles.section}>
+              <h1>Background</h1>
+              <p className={styles.headline}>
+                A passionate technology specialist with 15 years experience in
+                the industry, with a proven track record of learning new
+                technologies and guiding others in the delivery of high quality
+                software.
+              </p>
+              <div className={styles.achievements}>
+                <Slide direction="right" triggerOnce={true}>
+                  <h2>Achievements</h2>
+                  <div className={styles.techList}>
+                    <p>React Native</p>
+                    <p>React</p>
+                    <p>Typescript</p>
+                    <p>C#</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                    <p>React Native</p>
+                  </div>
+                </Slide>
+              </div>
+              <div>
+                <Slide direction="right" delay={500} triggerOnce={true}>
+                  <h2>Tech</h2>
+                  <div className={styles.techList}>
+                    <div>
+                      <Image
+                        src={"/react.png"}
+                        alt={"react"}
+                        height={175}
+                        width={175}
+                      ></Image>
+                      <p>React Native</p>
+                    </div>
+                    <div>
+                      <Image
+                        src={"/react.png"}
+                        alt={"react"}
+                        height={175}
+                        width={175}
+                      ></Image>
+                      <p>React Native</p>
+                    </div>
+                    <div>
+                      <Image
+                        src={"/react.png"}
+                        alt={"react"}
+                        height={175}
+                        width={175}
+                      ></Image>
+                      <p>React Native</p>
+                    </div>
+                    <div>
+                      <Image
+                        src={"/react.png"}
+                        alt={"react"}
+                        height={175}
+                        width={175}
+                      ></Image>
+                      <p>React Native</p>
+                    </div>
+                    <div>
+                      <Image
+                        src={"/react.png"}
+                        alt={"react"}
+                        height={175}
+                        width={175}
+                      ></Image>
+                      <p>React Native</p>
+                    </div>
+                  </div>
+                </Slide>
+              </div>
+            </div>
+          </Fade>
+          <Fade>
+            <div className={styles.section}>
+              <h1>Personal Projects</h1>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </div>
+          </Fade>
+          <Fade>
+            <div className={styles.section}>
+              <h1>Links</h1>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </div>
+          </Fade>
+          <Fade>
+            <div className={styles.section}>
+              <h1>Fun Stuff</h1>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </div>
+          </Fade>
         </div>
       </main>
     </div>
