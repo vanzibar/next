28dcdf3a7216f821e91a00875d6d1b189ec8f4f7
Evan Price <eprice@aforza.com>
2022-12-23 14:33:01 +0000

    more wip

--- next-webpage/pages/_app.tsx
+++ next-webpage/pages/_app.tsx
@@ -1,12 +1,23 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <title>Evan Price Personal Site</title>
+        <meta
+          name="description"
+          content="The personal site of Evan Price. Showcasing professional and personal software development projects."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
--- next-webpage/pages/index.tsx
+++ next-webpage/pages/index.tsx
@@ -1,133 +1,162 @@
 import { Fade, Slide } from "react-awesome-reveal";
 
-import Head from "next/head";
 import Image from "next/image";
+import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
+import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
+import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Evan Price Personal Site</title>
-        <meta
-          name="description"
-          content="The personal site of Evan Price. Showcasing professional and personal software development projects."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <div className={styles.headerWrapper}>
-          <h1 className={styles.title}>Hey There! I&apos;m Evan!</h1>
-          <Image
-            src={"/headshot.jpg"}
-            alt={"headshot"}
-            height={175}
-            width={175}
-            className={styles.headerImg}
-          ></Image>
-          <p className={styles.description}>
-            Seasoned software developer | Proven leader | Proud dad
-          </p>
-        </div>
-        <div>
-          <Fade>
-            <div className={styles.section}>
-              <h1>Background</h1>
-              <p className={styles.headline}>
-                A passionate technology specialist with 15 years experience in
-                the industry, with a proven track record of learning new
-                technologies and guiding others in the delivery of high quality
-                software.
+    <main className={styles.main}>
+      <div className={styles.headerWrapper}>
+        <h1 className={styles.title}>Hey There! I&apos;m Evan!</h1>
+        <Image
+          src={"/headshot.jpg"}
+          alt={"headshot"}
+          height={175}
+          width={175}
+          className={styles.headerImg}
+        ></Image>
+        <p className={styles.description}>
+          Seasoned software developer | Proven leader | Proud dad
+        </p>
+      </div>
+      <div>
+        <div className={styles.headlineWrapper}>
+          <Fade duration={2500}>
+            <div className={styles.headline}>
+              <p>
+                A software developer with a proven track record delivering of
+                high quality software.
               </p>
-              <div className={styles.achievements}>
-                <Slide direction="right" triggerOnce={true}>
-                  <h2>Achievements</h2>
-                  <div className={styles.techList}>
-                    <p>React Native</p>
-                    <p>React</p>
-                    <p>Typescript</p>
-                    <p>C#</p>
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>React Native</p>
-                  </div>
-                </Slide>
-              </div>
-              <div>
-                <Slide direction="right" delay={500} triggerOnce={true}>
-                  <h2>Tech</h2>
-                  <div className={styles.techList}>
-                    <div>
-                      <Image
-                        src={"/react.png"}
-                        alt={"react"}
-                        height={175}
-                        width={175}
-                      ></Image>
-                      <p>React Native</p>
-                    </div>
-                    <div>
-                      <Image
-                        src={"/react.png"}
-                        alt={"react"}
-                        height={175}
-                        width={175}
-                      ></Image>
-                      <p>React Native</p>
-                    </div>
-                    <div>
-                      <Image
-                        src={"/react.png"}
-                        alt={"react"}
-                        height={175}
-                        width={175}
-                      ></Image>
-                      <p>React Native</p>
-                    </div>
-                    <div>
-                      <Image
-                        src={"/react.png"}
-                        alt={"react"}
-                        height={175}
-                        width={175}
-                      ></Image>
-                      <p>React Native</p>
-                    </div>
-                    <div>
-                      <Image
-                        src={"/react.png"}
-                        alt={"react"}
-                        height={175}
-                        width={175}
-                      ></Image>
-                      <p>React Native</p>
-                    </div>
-                  </div>
-                </Slide>
-              </div>
-            </div>
-          </Fade>
-          <Fade>
-            <div className={styles.section}>
-              <h1>Personal Projects</h1>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </div>
-          </Fade>
-          <Fade>
-            <div className={styles.section}>
-              <h1>Links</h1>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </div>
-          </Fade>
-          <Fade>
-            <div className={styles.section}>
-              <h1>Fun Stuff</h1>
-              <p>Find in-depth information about Next.js features and API.</p>
             </div>
           </Fade>
         </div>
-      </main>
-    </div>
+        <div className={`${styles.section} ${styles.secondaryBackground}`}>
+          <Slide triggerOnce={true} duration={1500}>
+            <div className={styles.achievements}>
+              <div className={styles.achievement}>
+                <MilitaryTechIcon
+                  sx={{
+                    fontSize: "15rem",
+                  }}
+                ></MilitaryTechIcon>
+                <h3>Seasoned</h3>
+                <p>
+                  I have over <span>15 years commercial experience</span>,
+                  across various types of organisation; from startup, through to
+                  large multinationals.
+                </p>
+              </div>
+              <div className={styles.achievementImageWrapper}></div>
+            </div>
+          </Slide>
+          <div className={styles.achievements}>
+            <Slide triggerOnce={true} duration={1500} direction={"up"}>
+              <div className={styles.achievement}>
+                <WorkspacePremiumIcon
+                  sx={{
+                    fontSize: "15rem",
+                  }}
+                ></WorkspacePremiumIcon>
+                <h3>Educated</h3>
+                <p>
+                  I am educted to <span>Masters</span> level, with distinction.
+                </p>
+              </div>
+              <div className={styles.achievementImageWrapper}></div>
+            </Slide>
+          </div>
+          <div className={styles.achievements}>
+            <Slide triggerOnce={true} duration={1500} direction={"right"}>
+              <div className={styles.achievement}>
+                <VerifiedUserIcon
+                  sx={{
+                    fontSize: "15rem",
+                  }}
+                ></VerifiedUserIcon>
+                <h3>Certified</h3>
+                <p>
+                  I am a certified <span>Scrum Master</span> and advocate for
+                  Agile processes.
+                </p>
+              </div>
+              <div className={styles.achievementImageWrapper}></div>
+            </Slide>
+          </div>
+        </div>
+        <Fade>
+          <div className={styles.section}>
+            <div>
+              {/* <h2>Tech</h2> */}
+              <Slide
+                triggerOnce={true}
+                direction="right"
+                delay={500}
+                cascade={true}
+              >
+                <div className={styles.techList}>
+                  <div>
+                    <Image
+                      src={"/react.png"}
+                      alt={"react"}
+                      height={175}
+                      width={175}
+                    ></Image>
+                    <p>React Native</p>
+                  </div>
+                  <div>
+                    <Image
+                      src={"/react.png"}
+                      alt={"react"}
+                      height={175}
+                      width={175}
+                    ></Image>
+                    <p>Typescript/Javascript</p>
+                  </div>
+                  <div>
+                    <Image
+                      src={"/react.png"}
+                      alt={"react"}
+                      height={175}
+                      width={175}
+                    ></Image>
+                    <p>Redux</p>
+                  </div>
+                  <div>
+                    <Image
+                      src={"/react.png"}
+                      alt={"react"}
+                      height={175}
+                      width={175}
+                    ></Image>
+                    <p>HTML</p>
+                  </div>
+                  <div>
+                    <Image
+                      src={"/react.png"}
+                      alt={"react"}
+                      height={175}
+                      width={175}
+                    ></Image>
+                    <p>CSS</p>
+                  </div>
+                  <div>
+                    <Image
+                      src={"/react.png"}
+                      alt={"react"}
+                      height={175}
+                      width={175}
+                    ></Image>
+                    <p>C#</p>
+                  </div>
+                </div>
+              </Slide>
+            </div>
+          </div>
+        </Fade>
+      </div>
+    </main>
   );
 }
