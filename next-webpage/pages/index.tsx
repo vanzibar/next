daab78ad7054c9fd687789e7f21256ceb4f16563
Evan Price <eprice@aforza.com>
2023-01-11 08:47:44 +0000

    wip

--- next-webpage/pages/index.tsx
+++ next-webpage/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       </div>
       <div>
         <div className={styles.headlineWrapper}>
-          <Fade duration={2500}>
+          <Fade className="fade" duration={2500}>
             <div className={styles.headline}>
               <p>
                 A software developer with a proven track record delivering of
@@ -34,128 +34,194 @@ export default function Home() {
           </Fade>
         </div>
         <div className={`${styles.section} ${styles.secondaryBackground}`}>
-          <Slide triggerOnce={true} duration={1500}>
+          <Fade className={styles.fade}>
             <div className={styles.achievements}>
-              <div className={styles.achievement}>
-                <MilitaryTechIcon
-                  sx={{
-                    fontSize: "15rem",
-                  }}
-                ></MilitaryTechIcon>
-                <h3>Seasoned</h3>
-                <p>
-                  I have over <span>15 years commercial experience</span>,
-                  across various types of organisation; from startup, through to
-                  large multinationals.
-                </p>
-              </div>
-              <div className={styles.achievementImageWrapper}></div>
-            </div>
-          </Slide>
-          <div className={styles.achievements}>
-            <Slide triggerOnce={true} duration={1500} direction={"up"}>
-              <div className={styles.achievement}>
-                <WorkspacePremiumIcon
-                  sx={{
-                    fontSize: "15rem",
-                  }}
-                ></WorkspacePremiumIcon>
-                <h3>Educated</h3>
-                <p>
-                  I am educted to <span>Masters</span> level, with distinction.
-                </p>
-              </div>
-              <div className={styles.achievementImageWrapper}></div>
-            </Slide>
-          </div>
-          <div className={styles.achievements}>
-            <Slide triggerOnce={true} duration={1500} direction={"right"}>
-              <div className={styles.achievement}>
-                <VerifiedUserIcon
-                  sx={{
-                    fontSize: "15rem",
-                  }}
-                ></VerifiedUserIcon>
-                <h3>Certified</h3>
-                <p>
-                  I am a certified <span>Scrum Master</span> and advocate for
-                  Agile processes.
-                </p>
-              </div>
-              <div className={styles.achievementImageWrapper}></div>
-            </Slide>
-          </div>
-        </div>
-        <Fade>
-          <div className={styles.section}>
-            <div>
-              {/* <h2>Tech</h2> */}
               <Slide
+                className={styles.slide}
                 triggerOnce={true}
-                direction="right"
-                delay={500}
-                cascade={true}
+                duration={1500}
               >
-                <div className={styles.techList}>
-                  <div>
+                <div className={styles.achievement}>
+                  <MilitaryTechIcon
+                    sx={{
+                      fontSize: "15rem",
+                    }}
+                  ></MilitaryTechIcon>
+                  <h3>Seasoned</h3>
+                  <p>
+                    I have over <span>15 years commercial experience</span>,
+                    across various types of organisation; from startup, through
+                    to large multinationals.
+                  </p>
+                </div>
+                <div className={styles.achievementImageWrapper}></div>
+              </Slide>
+            </div>
+            <div className={styles.achievements}>
+              <Slide
+                className={styles.slide}
+                triggerOnce={true}
+                duration={1500}
+                direction={"up"}
+              >
+                <div className={styles.achievement}>
+                  <WorkspacePremiumIcon
+                    sx={{
+                      fontSize: "15rem",
+                    }}
+                  ></WorkspacePremiumIcon>
+                  <h3>Educated</h3>
+                  <p>
+                    I am educted to <span>Masters</span> level, with
+                    distinction.
+                  </p>
+                </div>
+                <div className={styles.achievementImageWrapper}></div>
+              </Slide>
+            </div>
+            <div className={styles.achievements}>
+              <Slide
+                className={styles.slide}
+                triggerOnce={true}
+                duration={1500}
+                direction={"right"}
+              >
+                <div className={styles.achievement}>
+                  <VerifiedUserIcon
+                    sx={{
+                      fontSize: "15rem",
+                    }}
+                  ></VerifiedUserIcon>
+                  <h3>Certified</h3>
+                  <p>
+                    I am a certified <span>Scrum Master</span> and advocate for
+                    Agile processes.
+                  </p>
+                </div>
+                <div className={styles.achievementImageWrapper}></div>
+              </Slide>
+            </div>
+          </Fade>
+        </div>
+        <div className={styles.section}>
+          <Fade className={styles.fade}>
+            <div className={styles.techSection}>
+              <h2>Chosen Tech</h2>
+              <div className={styles.techList}>
+                <div className={styles.techItem}>
+                  <div className={styles.techImageWrapper}>
                     <Image
                       src={"/react.png"}
                       alt={"react"}
                       height={175}
                       width={175}
                     ></Image>
-                    <p>React Native</p>
                   </div>
-                  <div>
+                  <p>React Native</p>
+                </div>
+                <div className={styles.techItem}>
+                  <div className={styles.techImageWrapper}>
                     <Image
-                      src={"/react.png"}
-                      alt={"react"}
-                      height={175}
-                      width={175}
+                      src={"/typescript-150.png"}
+                      alt={"typescript"}
+                      height={110}
+                      width={110}
                     ></Image>
-                    <p>Typescript/Javascript</p>
                   </div>
-                  <div>
+                  <p>TypeScript</p>
+                </div>
+                <div className={styles.techItem}>
+                  <div className={styles.techImageWrapper}>
                     <Image
-                      src={"/react.png"}
-                      alt={"react"}
-                      height={175}
-                      width={175}
+                      src={"/redux-150.png"}
+                      alt={"redux"}
+                      height={110}
+                      width={110}
                     ></Image>
-                    <p>Redux</p>
                   </div>
-                  <div>
+                  <p>Redux</p>
+                </div>
+                <div className={styles.techItem}>
+                  <div className={styles.techImageWrapper}>
                     <Image
-                      src={"/react.png"}
-                      alt={"react"}
-                      height={175}
-                      width={175}
+                      src={"/html-5-192.png"}
+                      alt={"HTML"}
+                      height={120}
+                      width={120}
                     ></Image>
-                    <p>HTML</p>
                   </div>
-                  <div>
+                  <p>HTML</p>
+                </div>
+                <div className={styles.techItem}>
+                  <div className={styles.techImageWrapper}>
                     <Image
-                      src={"/react.png"}
-                      alt={"react"}
-                      height={175}
-                      width={175}
+                      src={"/css3-150.png"}
+                      alt={"css"}
+                      height={135}
+                      width={135}
                     ></Image>
-                    <p>CSS</p>
                   </div>
-                  <div>
+                  <p>CSS</p>
+                </div>
+                <div className={styles.techItem}>
+                  <div className={styles.techImageWrapper}>
                     <Image
-                      src={"/react.png"}
-                      alt={"react"}
-                      height={175}
-                      width={175}
+                      src={"/c-sharp-logo-150.png"}
+                      alt={"csharp"}
+                      height={110}
+                      width={110}
                     ></Image>
-                    <p>C#</p>
+                  </div>
+                  <p>C#</p>
+                </div>
+              </div>
+            </div>
+          </Fade>
+        </div>
+        <div className={`${styles.section} ${styles.secondaryBackground}`}>
+          <Fade className={`${styles.fade} ${styles.highlights}`}>
+            <div>
+              <h2>Professional Highlights</h2>
+              <Slide
+                className={styles.slide}
+                triggerOnce={true}
+                direction={"right"}
+                duration={1500}
+              >
+                <div className={styles.highlight}>
+                  <Image
+                    src={"/aforza.jpg"}
+                    alt={"csharp"}
+                    height={554}
+                    width={792}
+                    className={styles.highlightImage}
+                  ></Image>
+                  <div>
+                    <h3>Aforza</h3>
+                    <ul>
+                      <li>
+                        I am a certified <span>Scrum Master</span> and advocate
+                        for Agile processes.
+                      </li>
+                      <li>
+                        I am a certified <span>Scrum Master</span> and advocate
+                        for Agile processes.
+                      </li>
+                      <li>
+                        I am a certified <span>Scrum Master</span> and advocate
+                        for Agile processes.
+                      </li>
+                      <li>
+                        I am a certified <span>Scrum Master</span> and advocate
+                        for Agile processes.
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </Slide>
             </div>
-          </div>
-        </Fade>
+          </Fade>
+        </div>
       </div>
     </main>
   );
